7b5ba581fa2245659a5c1f6dd7a9eb3e6170f7be
zaxanq <zaxanq@gmail.com>
2019-08-24 23:00:41 +0200

    Initial commit & setup

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'src',
+            routes: {
+                '/styles': 'src/styles/css'
+            }
+        },
+    });
+});
+
+gulp.task('sass', () => {
+    return gulp.src('src/styles/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('src/styles/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('watch', ['browserSync', 'sass'], () => {
+    gulp.watch('src/styles/scss/*.scss', ['sass']);
+    gulp.watch('src/styles/scss/*/*.scss', ['sass']);
+    gulp.watch('src/*.html', browserSync.reload);
+    gulp.watch('src/*.hbs', browserSync.reload);
+    gulp.watch('src/js/*.js', browserSync.reload);
+});
