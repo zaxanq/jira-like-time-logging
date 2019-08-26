a1557611824b507ecb629104f8c7440e50e77b4f
zaxanq <zaxanq@gmail.com>
2019-08-26 11:13:30 +0200

    Added dynamic column headers.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,42 @@
 class BaseClass {
     constructor() {
         this.init();
+        this.app();
     }
+
+    app() {
+        this.totalTime = {number: 480, string: '8h'};
+        this.Elements = {
+            columns: {
+                count: 0,
+                children: {
+                    0: {
+                        id: null,
+                        dayName: '',
+                        date: '',
+                        totalWorkedTime: null,
+                        totalTime: null,
+                        title: 'Worklogs',
+                    }
+                },
+                dayNames: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            cards: {
+                count: 0,
+                children: {
+                    0: {
+                        parentId: '',
+                        title: '',
+                        description: '',
+                        taskType: '',
+                        taskId: null,
+                        timeLogged: null,
+                    }
+                }
+            }
+        };
+    }
+
     init() {
         Element.prototype.addClass = function(className) {
             if (typeof className === 'string') {
@@ -28,6 +63,8 @@ class BaseClass {
             }
             return this;
         };
+
+        this.convertStringToTime('7h 30m');
     }
 
     Dom(selector) {
@@ -52,31 +89,86 @@ class BaseClass {
 
         return element;
     }
+
+    fixDate(number) {
+        if (number < 10) return `0${number}`;
+        return number;
+    }
+
+    convertStringToTime(string) {
+        string = string.replace(/\s/g, '').slice(0, string.length - 2).split('h');
+        return Number(string[0]) * 60 + Number(string[1]);
+    }
+
+    convertTimeToString() {}
 }
 
 const Base = new BaseClass;
 
+class CardClass extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    create() {
+        let newColumn = Base.createElement('log-card',
+            {'class': 'card', 'id': `card-${++Base.Elements.cards.count}`},
+            Base.class('wrapper')[0]);
+    }
+
+}
+customElements.define('log-card', CardClass);
+let Card = new CardClass;
+
 class ColumnClass extends HTMLElement {
     constructor() {
         super();
-        this.init();
+        this.week = this.setWeek();
     }
 
-    init() {
-        this.Elements = {
-            columns: {
-                count: 0
-            }
-        };
+    setWeek() {
+        let week = [];
+        this.now = new Date;
+
+        for (let i = 1; i < this.now.getDay(); i++) {
+           week.push(this.now.getDate() - this.now.getDay() + i);
+        }
+
+        if (week.length === 0) week.push(this.now.getDate());
+        for (let i = week.length; i < 7; i++) {
+            week.push((week[week.length - 1]%31) + 1);
+        }
+
+        return week;
+    }
+
+    countTotalTimeInColumn(columnId) {
+        return '7h 30m';
     }
 
     create() {
         let newColumn = Base.createElement('log-column',
-            {'class': 'column', 'id': `column-${++this.Elements.columns.count}`},
+            {'class': 'column', 'id': `column-${++Base.Elements.columns.count}`},
             Base.class('wrapper')[0]);
+
+        this.addToElementsObject();
 
         this.createHeadElements(newColumn);
         this.createBodyElements(newColumn);
+    }
+
+    addToElementsObject() {
+        Base.Elements.columns.children[Base.Elements.columns.count] = {
+            id: Base.Elements.columns.count,
+            dayName: Base.Elements.columns.dayNames[Base.Elements.columns.count - 1],
+            date: this.week[Base.Elements.columns.count - 1] + '.' + Base.fixDate(this.now.getMonth() + 1),
+            totalWorkedTime: this.countTotalTimeInColumn(Base.Elements.columns.count),
+            totalTime: '8h',
+            title: 'Worklogs',
+        };
+    }
+
+    setProgress () {
 
     }
 
@@ -91,8 +183,8 @@ class ColumnClass extends HTMLElement {
         let newColumnHeadDateDate = Base.createElement('span',
             {'class': 'column__date-date'}, newColumnHeadDate);
 
-        newColumnHeadDateDay.innerText = 'Mon ';
-        newColumnHeadDateDate.innerText = '26.08';
+        newColumnHeadDateDay.innerText = Base.Elements.columns.children[Base.Elements.columns.count].dayName + ' ';
+        newColumnHeadDateDate.innerText = Base.Elements.columns.children[Base.Elements.columns.count].date;
 
         let newColumnHeadHours = Base.createElement('p',
             {'class': 'column__hours'}, newColumnHeadDetails);
@@ -103,20 +195,20 @@ class ColumnClass extends HTMLElement {
         let newColumnHeadHoursTotal = Base.createElement('span',
             {'class': 'column__hours-total'}, newColumnHeadHours);
 
-        newColumnHeadHoursWorked.innerText = '7h 30m';
+        newColumnHeadHoursWorked.innerText = Base.Elements.columns.children[Base.Elements.columns.count].totalWorkedTime;
         newColumnHeadHoursOf.innerText = ' of ';
-        newColumnHeadHoursTotal.innerText = '8h';
+        newColumnHeadHoursTotal.innerText = Base.Elements.columns.children[Base.Elements.columns.count].totalTime;
 
         let newColumnHeadProgress = Base.createElement('div',
             {'class': 'column__progress'}, newColumnHead);
         let newColumnHeadProgressDone = Base.createElement('div',
             {'class': 'column__progress--done',
-                'id': `column-${this.Elements.columns.count}__progress--done`}, newColumnHeadProgress);
+                'id': `column-${Base.Elements.columns.count}__progress--done`}, newColumnHeadProgress);
 
         let newColumnHeadTitle = Base.createElement('h2',
             {'class': 'column__title'}, newColumnHead);
 
-        newColumnHeadTitle.innerText = 'Worklogs';
+        newColumnHeadTitle.innerText = Base.Elements.columns.children[Base.Elements.columns.count].title;
     }
 
     createBodyElements(newColumn) {
